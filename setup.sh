08eeb55143291c5dec7fa618eb7baec3baef7255
AKGV1 <97513152+AKGV1@users.noreply.github.com>
2022-08-07 10:50:55 -0400

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,27 @@
+echo ">> FETCHING UPSTREAM..."
+git clone https://github.com/AKGV1/CHOCOVC /CHOCOVC
+echo ">> INSTALLING REQUIREMENTS..."
+cd /CHOCOVC
+pip3 install -U -r requirements.txt
+echo ">> STARTING MUSIC PLAYER USERBOT..."
+clear
+echo "
+#     #                        
+##   ## #    #  ####  #  ####  
+# # # # #    # #      # #    # 
+#  #  # #    #  ####  # #      
+#     # #    #      # # #      
+#     # #    # #    # # #    # 
+#     #  ####   ####  #  ####  
+                               
+######                                  
+#     # #        ##   #   # ###### #####  
+#     # #       #  #   # #  #      #    # 
+######  #      #    #   #   #####  #    # 
+#       #      ######   #   #      #####  
+#       #      #    #   #   #      #   #  
+#       ###### #    #   #   ###### #    # 
+                                          
+MUSIC PLAYER USERBOT IS SUCCESSFULLY DEPLOYED!
+"
+python3 main.py
